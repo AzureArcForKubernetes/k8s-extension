c26d0d02ef72f17ec0b837bf956912acca285a01
longwan <longwan@microsoft.com>
2025-07-16 23:53:49 +0000

    update test with retry

--- testing/test/extensions/public/AzureMonitorHighScale.Tests.ps1
+++ testing/test/extensions/public/AzureMonitorHighScale.Tests.ps1
@@ -9,28 +9,28 @@ Describe 'Azure Monitor High Scale Mode Testing' {
         . $PSScriptRoot/../../helper/Helper.ps1
     }
 
-    It 'Creates the extension with high log scale mode and verifies DCE creation' {
-        # Create extension with high scale mode enabled
+    It 'Creates the extension with high log scale mode and checks that it onboards correctly' {
         az $Env:K8sExtensionName create -c $($ENVCONFIG.arcClusterName) -g $($ENVCONFIG.resourceGroup) `
             --cluster-type connectedClusters --extension-type $extensionType -n $extensionName `
-            --configuration-settings "amalogs.useAADAuth=true" "amalogs.enableHighLogScaleMode=true" --no-wait
-        $? | Should -BeTrue
+            --configuration-settings "amalogs.enableHighLogScaleMode=true" --no-wait
+        $? | Should -BeTrue        
 
-        # Verify extension creation
         $output = az $Env:K8sExtensionName show -c $($ENVCONFIG.arcClusterName) -g $($ENVCONFIG.resourceGroup) --cluster-type connectedClusters -n $extensionName
         $? | Should -BeTrue
 
         $extension = ($output | ConvertFrom-Json)
-        $extension | Should -Not -BeNullOrEmpty
-        
+        $isAutoUpgradeMinorVersion = $extension.autoUpgradeMinorVersion 
+        $isAutoUpgradeMinorVersion.ToString() -eq "True" | Should -BeTrue
+
         # Verify high scale mode configuration
         $settings = $extension.configurationSettings
         $settings.'amalogs.enableHighLogScaleMode' | Should -Be "true"
         $settings.'amalogs.useAADAuth' | Should -Be "true"
 
-        # Wait for extension to install
+        # Loop and retry until the extension installs
         $n = 0
-        do {
+        do 
+        {
             if (Has-ExtensionData $extensionName) {
                 break
             }
@@ -38,109 +38,31 @@ Describe 'Azure Monitor High Scale Mode Testing' {
             $n += 1
         } while ($n -le $MAX_RETRY_ATTEMPTS)
         $n | Should -BeLessOrEqual $MAX_RETRY_ATTEMPTS
-
-        # Verify DCE creation
-        $clusterLocation = (az resource show -g $($ENVCONFIG.resourceGroup) -n $($ENVCONFIG.arcClusterName) --resource-type "Microsoft.Kubernetes/connectedClusters" --query location -o tsv).ToLower()
-        $dceName = "MSCI-ingest-$clusterLocation-$($ENVCONFIG.arcClusterName)"
-        if ($dceName.Length -gt 43) {
-            $dceName = $dceName.Substring(0, 43)
-            # Remove trailing hyphen if present
-            if ($dceName.EndsWith("-")) {
-                $dceName = $dceName.Substring(0, $dceName.Length - 1)
-            }
-        }
-        
-        # Wait for DCE to be fully created and configured
-        $maxRetries = 30  # 5 minutes total
-        $retryCount = 0
-        $dceObj = $null
-        
-        while ($retryCount -lt $maxRetries) {
-            $dce = az monitor data-collection endpoint show -g $($ENVCONFIG.resourceGroup) -n $dceName 2>$null
-            if ($?) {
-                $dceObj = ($dce | ConvertFrom-Json)
-                if ($dceObj.properties.networkAcls.publicNetworkAccess -eq "Enabled") {
-                    break
-                }
-            }
-            Start-Sleep -Seconds 10
-            $retryCount++
-        }
-
-        $dceObj | Should -Not -BeNullOrEmpty
-        $dceObj.kind | Should -Be "Linux"
-        $dceObj.properties.networkAcls.publicNetworkAccess | Should -Be "Enabled"
     }
 
-    It "Verifies Data Collection Rule configuration for high scale mode" {
-        # Get the DCR name
-        $clusterLocation = (az resource show -g $($ENVCONFIG.resourceGroup) -n $($ENVCONFIG.arcClusterName) --resource-type "Microsoft.Kubernetes/connectedClusters" --query location -o tsv).ToLower()
-        $dcrName = "MSCI-$clusterLocation-$($ENVCONFIG.arcClusterName)"
-        if ($dcrName.Length -gt 64) {
-            $dcrName = $dcrName.Substring(0, 64)
-        }
-
-        # Wait for DCR to be fully created and configured
-        $maxRetries = 30  # 5 minutes total
-        $retryCount = 0
-        $dcrObj = $null
-        
-        while ($retryCount -lt $maxRetries) {
-            $dcr = az monitor data-collection rule show -g $($ENVCONFIG.resourceGroup) -n $dcrName 2>$null
-            if ($?) {
-                $dcrObj = ($dcr | ConvertFrom-Json)
-                if ($dcrObj.properties.dataSources.extensions -and 
-                    $dcrObj.properties.dataSources.extensions[0].streams) {
-                    break
-                }
-            }
-            Start-Sleep -Seconds 10
-            $retryCount++
-        }
-
-        $dcrObj | Should -Not -BeNullOrEmpty
-        $dcrObj.properties.dataSources.extensions | Should -Not -BeNullOrEmpty
-        $dcrObj.properties.dataSources.extensions[0].streams | Should -Not -BeNullOrEmpty
-
-        # Verify high scale mode streams configuration
-        $streams = $dcrObj.properties.dataSources.extensions[0].streams
-        $streams | Should -Contain "Microsoft-ContainerLogV2-HighScale"
-        $streams | Should -Contain "Microsoft-KubeEvents"
-        $streams | Should -Contain "Microsoft-KubePodInventory"
-        $streams | Should -Contain "Microsoft-KubeNodeInventory"
-        $streams | Should -Contain "Microsoft-KubePVInventory"
-        $streams | Should -Contain "Microsoft-KubeServices"
-        $streams | Should -Contain "Microsoft-KubeMonAgentEvents"
-        $streams | Should -Contain "Microsoft-InsightsMetrics"
-        $streams | Should -Contain "Microsoft-ContainerInventory"
-        $streams | Should -Contain "Microsoft-ContainerNodeInventory"
-        $streams | Should -Contain "Microsoft-Perf"
+    It "Performs a show on the extension" {
+        $output = az $Env:K8sExtensionName show -c $($ENVCONFIG.arcClusterName) -g $($ENVCONFIG.resourceGroup) --cluster-type connectedClusters -n $extensionName
+        $? | Should -BeTrue
+        $output | Should -Not -BeNullOrEmpty
     }
 
-    It "Deletes the extension and verifies DCE cleanup" {
-        # Get DCE name before deletion
-        $clusterLocation = (az resource show -g $($ENVCONFIG.resourceGroup) -n $($ENVCONFIG.arcClusterName) --resource-type "Microsoft.Kubernetes/connectedClusters" --query location -o tsv).ToLower()
-        $dceName = "MSCI-ingest-$clusterLocation-$($ENVCONFIG.arcClusterName)"
-        if ($dceName.Length -gt 43) {
-            $dceName = $dceName.Substring(0, 43)
-            # Remove trailing hyphen if present
-            if ($dceName.EndsWith("-")) {
-                $dceName = $dceName.Substring(0, $dceName.Length - 1)
-            }
-        }
+    It "Lists the extensions on the cluster" {
+        $output = az $Env:K8sExtensionName list -c $($ENVCONFIG.arcClusterName) -g $($ENVCONFIG.resourceGroup) --cluster-type connectedClusters
+        $? | Should -BeTrue
 
-        # Delete the extension
+        $output | Should -Not -BeNullOrEmpty
+        $extensionExists = $output | ConvertFrom-Json | Where-Object { $_.extensionType -eq $extensionType }
+        $extensionExists | Should -Not -BeNullOrEmpty
+    }
+
+    It "Deletes the extension from the cluster" {
         $output = az $Env:K8sExtensionName delete -c $($ENVCONFIG.arcClusterName) -g $($ENVCONFIG.resourceGroup) --cluster-type connectedClusters -n $extensionName --force
         $? | Should -BeTrue
 
-        # Verify extension is deleted
+        # Extension should not be found on the cluster
         $output = az $Env:K8sExtensionName show -c $($ENVCONFIG.arcClusterName) -g $($ENVCONFIG.resourceGroup) --cluster-type connectedClusters -n $extensionName
         $? | Should -BeFalse
         $output | Should -BeNullOrEmpty
-
-        # Verify DCE is deleted
-        $dce = az monitor data-collection endpoint show -g $($ENVCONFIG.resourceGroup) -n $dceName
-        $? | Should -BeFalse
     }
 
     It "Performs another list after the delete" {
